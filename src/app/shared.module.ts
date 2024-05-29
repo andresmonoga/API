e9c75ff30607149a546ac8d87f244744f4f98a5d
Andres Monoga <andresmonoga07@gmail.com>
2024-05-29 11:35:07 -0500

    API

---
+++ src/app/shared.module.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { SearchBoxComponent } from './shared/components/search-box/search-box.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    
+     AboutPageComponent,
+     ContactPageComponent,
+     HomePageComponent,
+     SidebarComponent,
+     SearchBoxComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+     AboutPageComponent,
+     ContactPageComponent,
+     HomePageComponent,
+     SidebarComponent,
+     SearchBoxComponent,
+  ] 
+})
+export class SharedModule { }
